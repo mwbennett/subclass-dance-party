82454be11f7d12cde482251111aef0ea5fb3b31a
Mark Bennett <mwbennett@wesleyan.edu>
2015-03-31 11:24:40 -0700

    Fixed mouseover function

--- src/mouseyDancer.js
+++ src/mouseyDancer.js
@@ -6,7 +6,22 @@ MouseyDancer.prototype = Object.create(BouncyDancer.prototype);
 MouseyDancer.prototype.constructor = MouseyDancer;
 
 MouseyDancer.prototype.react = function(){
-  $('.dancer').on('mouseover', function(){
-    this.css('background-color', 'blue');
+  this.$node.on('mouseover', function(){
+    $(this).css('border','20px solid limegreen');
   });
 }
+
+BouncyDancer.prototype.step = function(){
+  this.oldStep();
+  // call the old version of step at the beginning of any call to this new version of step
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  // this.bounceUp();
+  // setTimeout(null,1000);
+  // this.bounceDown();
+  if (!this.pause){
+    this.bounce();
+    this.react();
+  }
+};
